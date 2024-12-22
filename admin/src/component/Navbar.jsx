c186def8a38eba5dd8f111642c2097ebcab10b0b
MohamedAmr23 <amrm96754@gmail.com>
2024-12-22 04:35:27 +0200

    setup add page

--- admin/src/component/Navbar.jsx
+++ admin/src/component/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import {assets} from '../assets/assets.js'
-const Navbar = () => {
+import { useNavigate } from 'react-router-dom'
+const Navbar = ({setToken}) => {
+  const navigate = useNavigate()
+  const logOut = ()=>{
+    setToken('')
+    navigate('/login')
+  }
   return (
     <div className='flex items-center justify-between py-2 px-[4%]'>
         <img className='w-[max(10%,80px)]' src={assets.logo} alt='' />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={logOut} className='bg-gray-600 text-white px-5 py-2 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
